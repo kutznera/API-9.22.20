a0f999c406b406e1fe6f629a5292eeabd0b53b2a
kutznera <kutznera15@gmail.com>
2020-09-23 19:03:03 -0700

    Corrections and updates

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const dialogflow = require("@google-cloud/dialogflow");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
@@ -9,52 +8,63 @@ dotenv.config({ path: "./config/config.env" });
 app.use(express.static(path.join(_dirname, "views")));
 app.use(express.static(path.join(_dirname, "public")));
 
-const por = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(
-    port,
+    PORT,
     console.log(
-        'server is runing on ${process.env.NODE_ENV} mode at port {PORT}'.yellow.bold
+        `server is running on ${process.env.NODE_ENV} mode at port {PORT}`.yellow
+        .bold
     )
 );
 
+const dialogflow = require("@google-cloud/dialogflow");
+
 const io = socketio(server);
-io.on("connection", function(socket))
-console.log("a user connected");
+io.on("connection", function(socket) {
+    console.log("a user connected");
 
-socket.on("chat message", (message) => {
-    console.log(message);
+    socket.on("chat message", (message) => {
+        console.log(message);
 
-    const callapibot = async(projectId = process.env.PROJECT_ID) => {
-        try {
-            const sessionId = uuid.v4();
-            const sessionClient = new dialogflow.SessionsClient({
-                keyfilename: "./WebSpeechAIbot-41194528781e.json",
-            });
-            const sessionPOath,
-                queryInput: {
-                    text: {
-                        text: message,
-                        languageCode: "en-US",
-                    }
+        const callapibot = async(projectId = process.env.newagent - euug) => {
+            try {
+                const sessionId = uuid.v4();
+                const sessionClient = new dialogflow.SessionsClient({
+                    keyfilename: "./WebSpeechAIbot-41194528781e.json",
+                });
+                const sessionPath, = sessionClient.projectAgentSessionPath(
+                    projectId,
+                    sessionId
+                );
+                const request = {
+                    session: sessionPath,
+                    queryInput: {
+                        text: {
+                            text: message,
+                            languageCode: "en-US",
+                        },
+                    },
+                };
+                const responses = await sessionClient.detectIntent(request);
+
+                const result = responses[0].queryResult.fulfillmentText;
+
+                socket.emit("bot reply", result);
+
+                if (result.intent) {
+                    console.log('Intent: (result.intent.displayName}');
+                } else {
+                    console.log("No intent matched.");
                 }
-        }
-        const responses = await sessionClient.detectIntent(request);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-        const result = responses[0].queryResult.fulfillmentText;
-
-        socket.emit("bot reply", result);
-
-        if (result.intent) {
-            console.log('Intent: (result.intent.displayName}');
-        } else {
-            console.log("No intent matched.");
-        }
-    } catch (error) {
-        console.log(error);
-    }
+        callapibot();
+    });
 });
 
 callapibot();
 });
-})
--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 const btn = document.querySelector("button");
-cont outputme = document.querySelector(".output-you");
+const outputme = document.querySelector(".output-you");
 const outputbot = document.querySelector(".output-bot");
 
 const SpeechRecognition =
-    window.SpeechRecognition = || window.webkitSpeechRecognition;
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 
-recognition.lan = "en=US";
+recognition.lang = "en-US";
 recognition.interimResults = false;
 
 btn.addEventListener("click", () => {
     recognition.start();
 });
-console.log("imadeit");
+
 recognition.onresult = function(event) {
     const last = event.results.length - 1;
     const text = event.results[last][0].transcript;
@@ -22,7 +22,11 @@ recognition.onresult = function(event) {
     outputme.textContent = text;
 };
 
+
 const socket = io();
+
+socket.emit("chat message", text);
+
 
 const botReply = (text) => {
     const synth = window.speechSynthesis;
